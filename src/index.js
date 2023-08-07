afb22f68e902b0f9b6773a92deb54ff6c429dc97
root <+>
2023-08-07 14:59:29 -0600

    submit

--- src/index.js
+++ src/index.js
@@ -1,5 +1,36 @@
+
+
+// add event listener to capture form data
+// prevent default
+// fetch data based on what user put into the form
+// display that data on the page
+
+
 const init = () => {
+
+    const inputForm = document.querySelector("form")
+    inputForm.addEventListener("submit", (event) => {
+        event.preventDefault()
+        const input = document.querySelector("input#searchByID")
+
+        console.log(input.value)
+        
+        fetch(`http://localhost:3000/movies/${input.value}`)
+        .then((response) => response.json())
+        .then((data) => {
+            const title = document.querySelector("section#movieDetails h4")
+            const summary = document.querySelector("section#movieDetails p")
+        
+            title.innerText = data.title
+            summary.innerText = data.summary
+        
+        })
+
+
+    })
+
   
 }
 
-document.addEventListener('DOMContentLoaded', init);
+
+document.addEventListener('DOMContentLoaded', init); 
